0e6e137f5399144f897e5e9c9c8e3d2e772e35b8
ridamm <ridam.maheshwari.3@gmail.com>
2018-09-26 15:08:38 +0530

    Added class for frames

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 import socket
+from frame import *
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+d1=dataframe(100,0,"Hello")
+print(d1.data)
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.sendall(b'Hello, world')
+#     data = s.recv(1024)
 
-print('Received', repr(data))
+# print('Received', repr(data))
---
+++ frame.py
@@ -0,0 +1,12 @@
+class dataframe:
+	"""docstring for dataframe"""
+	def __init__(self, length, index, data):
+		self.length=length
+		self.data=data
+		self.index=index
+
+class ackframe:
+	"""docstring for ackframe"""
+	def __init__(self, length, index ):
+		self.length=length
+		self.index=index
