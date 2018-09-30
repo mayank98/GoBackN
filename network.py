f1580f9157ecc8abb442520b4d4476f522a403ba
Mayank Gupta <mayankgupta18198@gmail.com>
2018-09-30 21:24:05 +0530

    added network
    Signed-off-by: Mayank Gupta <mayankgupta18198@gmail.com>

---
+++ network.py
@@ -0,0 +1,18 @@
+from socket import *
+from random import *
+from frame import *
+
+drop_prob_dataframe = 0.1
+drop_prob_ackframe = 0.05
+
+
+def transmit(in_socket,message,address):
+    
+    drop_prob=0
+    if (message.__class__.__name__=="ackframe"):
+        drop_prob=drop_prob_ackframe
+    else:
+        drop_prob=drop_prob_dataframe
+
+    if (random() > drop_prob):
+        in_socket.sendto(message,address)
